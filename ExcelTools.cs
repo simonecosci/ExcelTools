408dc336b2b9f339228fca4cd479c7a7557be1fd
Simone Cosci <simone.cosci@gmail.com>
2019-03-05 16:56:57 +0100

    Add files via upload

---
+++ ExcelTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using ExcelTools.Drivers;
+
+namespace ExcelTools
+{
+    public class ExcelTool
+    {
+        protected string filepath;
+
+        protected IExcelTools driver;
+
+        public ExcelTool(string filepath) {
+            this.filepath = filepath;
+        }
+
+        public void FromDataTable(DataTable data) {
+            driver.FromDataTable(data);
+        }
+
+        public DataTable ToDataTable() {
+            return driver.ToDataTable();
+        }
+
+        public void SetDriver(string driverName) {
+            IExcelTools driverObject;
+            switch (driverName) {
+
+                case "EPPlus":
+                    throw new Exception("Not implemented yet");
+
+                case "NPOI":
+                default:
+                    driverObject = new NPOIDriver(filepath);
+                    break;
+            }
+            driver = driverObject;
+        }
+
+    }
+}
